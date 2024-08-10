01bf17fa731dbe668c8365e49b80e6c4992609e3
1F616EMO <root@1f616emo.xyz>
2024-08-10 08:18:37 +0800

    Use JPG

--- init.lua
+++ init.lua
@@ -16,18 +16,18 @@ teacher.register_turorial("teacher_tutorial_elevator:use_elevator", {
     },
 
     {
-        texture = "teacher_tutorial_elevator_teacher_1.png",
+        texture = "teacher_tutorial_elevator_teacher_1.jpg",
         text =
             S("Elevators transport players vertically. To use an elevator, walk inside an elevator and right-click it."),
     },
     {
-        texture = "teacher_tutorial_elevator_teacher_2.png",
+        texture = "teacher_tutorial_elevator_teacher_2.jpg",
         text =
             S("After right-clicking, select a destination from the list of locations. " ..
                 "The number left of the name is the Y-axis coordinate of the destination."),
     },
     {
-        texture = "teacher_tutorial_elevator_teacher_3.png",
+        texture = "teacher_tutorial_elevator_teacher_3.jpg",
         text =
             S("To set up elevators, place down elevators so they are vertically aligned. " ..
                 "Then, place shafts to fill in any gaps between elevators. " ..
